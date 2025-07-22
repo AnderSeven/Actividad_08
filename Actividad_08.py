08fc2815601a2c6733936be527e4e9b757b77403
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-07-21 22:59:16 -0600

    avances en el menu

--- Actividad_08.py
+++ Actividad_08.py
@@ -1,1 +1,24 @@
-#asdf
+opciones = 0
+a = False
+while a == False:
+    print("===Menu===")
+    print("1. Calcular el factorial de un numero")
+    print("2. Suma de los primeros n numeros naturales")
+    print("3. Calcular el n-esimo numero de Fibonacci")
+    print("4. Contar cuentas veces aparece una letra en una palabra")
+    print("5. Invertir una cadena de texto")
+    print("6. Calcular la potencia de un numero (base ^ exponente)")
+    print("7. Salir del programa")
+    opciones = int(input("Elija una opcion: "))
+    match opciones:
+        case 1:
+            print()
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+            print("Gracias por usar el sistema")
+        case _:
+            print("Opcion invalida")
